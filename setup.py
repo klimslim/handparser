c276c4df0e9e7cc119725b5d3b764f8d46978d42
Walkman <kissgyorgy@me.com>
2014-03-19 07:02:59 +0100

    Bump version number.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     author='Walkman',
     name="handparser",
-    version='0.7.1',
+    version='0.7.3',
     py_modules=['handparser'],
     install_requires=['pytz', 'pytest==2.4.2', 'coveralls'],
 )
